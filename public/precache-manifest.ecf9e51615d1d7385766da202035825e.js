fbae7c548f6a3f5f0e69330b5887e29988ab7567
Kush Guglani <kush.guglani@nagarro.com>
2020-06-12 15:15:57 +0530

    initial commit

---
+++ public/precache-manifest.ecf9e51615d1d7385766da202035825e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04f3d89136b36490c78a031ce1f9fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbd1b7288986a020920",
+    "url": "/static/css/main.e3e15cef.chunk.css"
+  },
+  {
+    "revision": "509b36bb17739861f270",
+    "url": "/static/js/2.f188c648.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.f188c648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbd1b7288986a020920",
+    "url": "/static/js/main.79550900.chunk.js"
+  },
+  {
+    "revision": "5553ae6670842f2f1cd4",
+    "url": "/static/js/runtime-main.bbf94d4a.js"
+  }
+]);
